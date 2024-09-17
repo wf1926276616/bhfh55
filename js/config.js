11ae5151bd85f463471c4b3802798d7cfd580381
wf1926276616 <122534405+wf1926276616@users.noreply.github.com>
2024-09-18 06:03:40 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,20 @@
+var BaseUrl =  "http://103.8.69.108:1663";
+var ResourceUrl = 'http://103.8.69.108:1999/ttxinshenshou/';
+
+if(window.location.pathname!='/'
+    &&window.location.pathname!='/index.html'
+    &&window.location.pathname!='/site/login.html'
+    &&window.location.pathname!='/site/wxredd.html')
+{
+    var wwid=window.localStorage.getItem("wwid");
+    if(!wwid)
+    {
+        localStorage.setItem('enter_url',encodeURIComponent(window.location.href))
+
+        alert('请先登录');
+        window.location.href = '/index.html';
+    }
+}
+
+
+        
